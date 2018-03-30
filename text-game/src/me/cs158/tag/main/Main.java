41170435ef48ebc34c2958b4c5a188714f5f2319
Preston <prestonpetrie@gmail.com>
2018-03-30 15:48:24 -0500

    Un-commented necessary line

--- text-game/src/me/cs158/tag/main/Main.java
+++ text-game/src/me/cs158/tag/main/Main.java
@@ -5,7 +5,7 @@ import me.cs158.tag.player.Wizard;
 
 public class Main {
 	
-	//public static enum SUCCESS { TRUE, FALSE, PARTIAL }
+	public static enum SUCCESS { TRUE, FALSE, PARTIAL }
 	
 	public static void main(String[] args) {
 		Knight terry = new Knight("Terry", 100,3);
