d1c9099a8031f0094a84ef450b3c9cbac8d83d79
Terry <Terry@152.228.117.239>
2018-03-30 21:01:27 -0500

    stuff

--- text-game/src/me/cs158/tag/player/Knight.java
+++ text-game/src/me/cs158/tag/player/Knight.java
@@ -1,12 +1,12 @@
 package me.cs158.tag.player;
 
+import me.cs158.tag.item.MeleeWeapon;
+
 public class Knight extends Player{
 	private int shieldBashes;
 	
 	public Knight(String name, int hp, int m) {
 		super(name, hp, m);
-		//this.inventory.addItem(new MeleeWeapon("Sword")); will add a knightly broadsword to his inventory
-		//this.inventory.addItem(new Item("Shield")); will add a knightly shield to his inventory
 	}
 	
 	@Override
